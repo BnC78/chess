84bad27083ef86681eeaaffe7b2192b95637db6e
BnC78 <ujvari.bence.2000@gmail.com>
2021-05-24 17:31:20 +0200

    board creation implemented

--- game/Board.java
+++ game/Board.java
@@ -1,10 +1,24 @@
 package game;
+
+import javax.swing.JButton;
 import javax.swing.JPanel;
+import java.awt.GridLayout;
 
 public class Board extends JPanel {
 
     public Board() {
-
+        createBoard();
     }
 
+    private void createBoard() {
+        for (int i = 8; i >= 1; ++i) {
+            for (int j = (int)'a'; j <= (int)'h'; ++j) {
+                String tile = "" + (char) i + j;
+                JButton button = new JButton(tile);
+                this.add(button);
+            }
+        }
+        this.setLayout(new GridLayout(8, 8));
+        this.setVisible(true);
+    }
 }
